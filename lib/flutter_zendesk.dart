58a9d8b81ecd3334fb57750aee61f2c1fd9471b2
Leonid Demyanenko <leonid.demyanenko@green-light.com>
2020-03-15 13:12:42 +0200

    dart notification methods

--- lib/flutter_zendesk.dart
+++ lib/flutter_zendesk.dart
@@ -10,6 +10,14 @@ class FlutterZendesk {
     return await _channel.invokeMethod('initiate', params);
   }
 
+  static Future<String> initNotifications({@required Map<String, dynamic> params}) async {
+    return await _channel.invokeMethod('initNotifications', params);
+  }
+
+  static Future<String> openTicket({@required Map<String, dynamic> params}) async {
+    return await _channel.invokeMethod('openTicket', params);
+  }
+
   static Future<String> openHelpCenter({@required Map<String, dynamic> params}) async {
     return await _channel.invokeMethod('help', params);
   }
